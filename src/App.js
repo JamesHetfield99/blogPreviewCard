8f2b0eb6f67194e05c3486ce60ace1bfabde5309
KobakhidzeDimitri1 <dimitri.kobakhidze.1@btu.edu.ge>
2023-12-25 03:33:27 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import avatarPhoto from "./images/image-avatar.webp"
+import topPhoto from "./images/illustration-article.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='image-wrapper'>
+        <img src={topPhoto} alt='header' />
+      </div>
+      <span className="learning-span">Learning</span>
+      <span className="publish-span">Published 21 Dec 2023</span>
+      <h1 className="header-text">HTML & CSS foundations</h1>
+      <p className="paragraph-1">
+        These languages are the backbone of every website, defining structure, content, and presentation.
+      </p>
+      <div className="person-wrapper">
+        <img src={avatarPhoto} alt="person" />
+        <span className="name-span">Greg Hooper</span>
+      </div>
+    </main>
   );
 }
 
